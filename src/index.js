2958576786ee93b09e474312d72287ec8bf87117
schmelczerandras <schmelczer.andras@simonyi.bme.hu>
2020-10-13 21:23:19 +0200

    Update to match new API

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
-import { vec2, vec3 } from "gl-matrix";
-import { compile, Circle, CircleLight } from "sdf-2d";
+import { vec2 } from "gl-matrix";
+import { compile, CircleFactory, rgb, hsl, CircleLight } from "sdf-2d";
 
 const main = async () => {
   const canvas = document.querySelector("canvas");
+
+  // Set the circle to use the zeroth element of the colorPalette
+  const Circle = CircleFactory(0);
 
   const renderer = await compile(canvas, [
     Circle.descriptor,
     CircleLight.descriptor,
   ]);
 
-  renderer.setRuntimeSettings({
-    colorPalette: [
-      vec3.create(),
-      vec3.create(),
-      vec3.fromValues(0.5, 0.2, 1), // By default, Circle has a colorIndex of 2
-    ],
-  });
+  const setHue = (hue) =>
+    renderer.setRuntimeSettings({
+      colorPalette: [hsl(hue, 100, 50)],
+    });
 
   let aspectRatio;
   const setViewArea = () => {
     const canvasSize = renderer.canvasSize;
     aspectRatio = canvasSize.x / canvasSize.y;
 
-    // The view area is given in a coordinate system
-    // originated from the bottom (!) left edge of the canvas
+    // The view area is given in a coordinate system originated
+    // from the bottom (!) left edge of the canvas
     renderer.setViewArea(
       vec2.fromValues(0, 1),
       vec2.fromValues(aspectRatio, 1)
@@ -34,13 +34,10 @@ const main = async () => {
   onresize = setViewArea;
 
   const circle = new Circle(vec2.fromValues(0.5, 0.5), 0.1);
-  const light = new CircleLight(
-    vec2.create(),
-    vec3.fromValues(1, 0.5, 0.1),
-    0.0005
-  );
+  const light = new CircleLight(vec2.create(), rgb(1, 0.5, 0.1), 0.0005);
 
   const animate = (currentTime) => {
+    setHue(((currentTime / 4000) * 360) % 360);
     vec2.set(circle.center, aspectRatio / 2, 0.5);
     vec2.set(
       light.center,
@@ -52,7 +49,7 @@ const main = async () => {
     renderer.addDrawable(light);
     renderer.renderDrawables();
 
-    console.log(renderer.insights);
+    console.info(renderer.insights);
 
     requestAnimationFrame(animate);
   };
